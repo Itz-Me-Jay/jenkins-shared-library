a19cba0b7225ed5e4a42bbfb48e636544b4a7180
Valluri Sri Jayasurya <v.srijayasurya@gmail.com>
2023-07-24 21:36:38 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,11 +1,10 @@
-def call(Map stepsParams) {
-    // Store Git credentials using the withCredentials block
+def call(Map stageParams) {
+// Store Git credentials using the withCredentials block
     // withCredentials([usernamePassword(credentialsId: 'my-credentials-id', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-        // Call gitcheckout using "MAP" variables for parameters like branch and URL     
-        checkout([
-            $class: 'GitSCM',
-            branches: [[name: stageParams.branch]],
-            userRemoteConfig: [[url: stageParams.url]]
-        ])
-    // }
-}
+    // Call gitcheckout using "MAP" variables for parameters like branch and URL  
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
