544423a438909ce3dbe49196e8b9d460b5475235
Valluri Sri Jayasurya <v.srijayasurya@gmail.com>
2023-07-25 00:08:38 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call(credentialsId){
-
+    // // name of the credentials ID is sonartoken-api ( which we created in credentials ) we are calling credentialsID in def call ()
     withSonarQubeEnv(credentialsId: credentialsId) {
          sh 'mvn clean package sonar:sonar'
     }
