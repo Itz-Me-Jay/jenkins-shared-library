fc37b4511722e261d72f89679b91cd6797eb59a6
Valluri Sri Jayasurya <v.srijayasurya@gmail.com>
2023-07-25 19:43:35 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,17 +1,18 @@
-// def call(String project, String ImageTag, String hubUser){
+def call(String project, String ImageTag, String hubUser){
   
-//     sh """
-//      docker image build -t ${hubUser}/${project} . 
+       sh """
+        docker image build -t ${hubUser}/${project} . 
         // docker image build -t{account}/${prjectname}
-//      docker image tag ${hubUser}/${project3} ${hubUser}/${project3}:${ImageTag}
-//      docker image tag ${hubUser}/${project3} ${hubUser}/${project3}:latest  
-//     """
-// }
+        docker image tag ${hubUser}/${project3} ${hubUser}/${project3}:${ImageTag}
+        docker image tag ${hubUser}/${project3} ${hubUser}/${project3}:latest  
+       """
+  }
 
-def call(String aws_account_id, String region, String ecr_repoName){
+/* def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
      docker build -t ${ecr_repoName} .
      docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
+*/
