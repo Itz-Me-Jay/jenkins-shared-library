deb114c964fef6ce88001ca5f14befc6759c7afe
Valluri Sri Jayasurya <v.srijayasurya@gmail.com>
2023-07-25 20:10:51 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(String project, String ImageTag, String hubUser){
   
        sh """
-        docker image build -t ${hubUser}/${project} . 
+        docker image build -t ${hubUser}/${project3} . 
         docker image tag ${hubUser}/${project3} ${hubUser}/${project3}:${ImageTag}
         docker image tag ${hubUser}/${project3} ${hubUser}/${project3}:latest  
        """
